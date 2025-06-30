e45ad68697627d35cfe91b314dd972a88044171f
AN-MBANK <mastershining000@example.com>
2025-06-30 16:31:56 +0700

    Update lesson11-ôn tập

--- lesson11-ôn tập/MaiBinhAn_2310900001/obj/Release/net8.0/MaiBinhAn_2310900001.AssemblyInfo.cs
+++ lesson11-ôn tập/MaiBinhAn_2310900001/obj/Release/net8.0/MaiBinhAn_2310900001.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaiBinhAn_2310900001")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69d4b9a6612e27453d085dc02fc747a239c127de")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaiBinhAn_2310900001")]
